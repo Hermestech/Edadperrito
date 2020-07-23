517d22c0964484330b9970575a23ca0abc32a54c
Hermes <adanhermes23@gmail.com>
2020-07-23 01:10:43 -0500

    Tarjeta, js agregados y función debuggeada!

--- edad.py
+++ edad.py
@@ -2,5 +2,5 @@ import math
 import cmath
 
 edad = int(input('¡Hola! por favor dime cuántos años tiene tu perro: '))
-edadp = 16 * int(math.log(edad)) + 31
-print('Tu perro tiene: ',edadp, 'años humanos' )
+edadp = 16 * math.log(edad) + 31
+print('Tu perro tiene: ',int(edadp), 'años humanos' )
---
+++ variables.js
@@ -0,0 +1,24 @@
+function edadPerrito(edad) {
+    console.log(`Tu perrito tiene ${edad} años` )
+}
+
+function sumar(a,b) {
+    var resultado = a + b;
+    return resultado;
+}
+
+var miNombre = "Hermes
+
+function nombre() {
+    var miApellido = "Aguilar";
+    console.log(miNombre + " " + miApellido)
+}
+
+nombre();
+
+function edadPerro(edad) {
+    var age = 16*Math.log(edad)+31;
+    return parseInt(age)
+}
+
+edadPerro();
